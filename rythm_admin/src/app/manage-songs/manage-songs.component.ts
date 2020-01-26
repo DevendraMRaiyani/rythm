08e9e2c7ec64cb3fcfe7861b0bca81ff25155aec
Kumar-Sondarva <jaysondarva4@gmail.com>
2020-01-26 18:46:58 +0530

    songs added with name and jpg file

--- rythm_admin/src/app/manage-songs/manage-songs.component.ts
+++ rythm_admin/src/app/manage-songs/manage-songs.component.ts
@@ -12,6 +12,8 @@ export class ManageSongsComponent implements OnInit {
   catagories
   playlists
   searchText;
+  imgurl:String= null;
+  fileToUpload:File=null;
   songs;
   // heroes = [
   //   { id: 11, name: 'Mr. Nice', country: 'India' },
@@ -37,6 +39,31 @@ export class ManageSongsComponent implements OnInit {
     }
   }
 
+
+
+  handleFileInput(file:FileList){
+    this.fileToUpload = file.item(0)
+    var reader = new FileReader()
+    reader.onload = (event: any) => {
+      this.imgurl = event.target.result;
+      console.log(this.imgurl)
+    }
+    reader.readAsDataURL(this.fileToUpload);
+
+  }
+
+  addSong(event)
+  {
+    event.preventDefault()
+    const target = event.target;
+    //const cname = target.querySelector('#sname').value;
+    const obj = {
+      name : target.querySelector('#sname').value,
+      photo : this.imgurl
+    }
+    this.http.post(`http://localhost:8080/song/add`,obj).subscribe(res => console.log('Done'));
+    //this.http.get("http://localhost:8080/checkCatagory?cname="+cname).subscribe((data) => this.checkCatagory(data,cname));
+  }
   loadSongs(data)
   {
     var x;
