9f5b3d98276d2defb219dc529280ec9ca010e182
Kumar-Sondarva <jaysondarva4@gmail.com>
2020-02-05 22:10:23 +0530

    File Upload Done successfully

--- rythm_admin/src/app/manage-songs/manage-songs.component.ts
+++ rythm_admin/src/app/manage-songs/manage-songs.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-
+import {  FileUploader, FileSelectDirective } from 'ng2-file-upload';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { map } from 'rxjs/operators';
+const UploadURL = 'http://localhost:8080/api/upload';
 @Component({
   selector: 'app-manage-songs',
   templateUrl: './manage-songs.component.html',
   styleUrls: ['./manage-songs.component.css']
 })
 export class ManageSongsComponent implements OnInit {
+  public uploader: FileUploader = new FileUploader({url: UploadURL, itemAlias: 'Song'});
   catagories
   playlists
   searchText;
@@ -16,7 +20,9 @@ export class ManageSongsComponent implements OnInit {
   fileToUpload:File=null;
   songs;
   rename=""
+  filestatus:number;
   public isCollapsed = true;
+  audioname:String;
   // heroes = [
   //   { id: 11, name: 'Mr. Nice', country: 'India' },
   //   { id: 12, name: 'Narco' , country: 'USA'},
@@ -29,7 +35,7 @@ export class ManageSongsComponent implements OnInit {
   //   { id: 19, name: 'Magma' , country: 'South Africa'},
   //   { id: 20, name: 'Tornado' , country: 'Sri Lanka'}
   // ];
-  constructor(public http:Http,public router:Router,public cookie:CookieService) { }
+  constructor(private http:Http,private router:Router,private cookie:CookieService) { }
 
   ngOnInit() {
     if(!(this.cookie.check("Adminuname") && this.cookie.check("Adminuid")))
@@ -39,38 +45,63 @@ export class ManageSongsComponent implements OnInit {
       this.http.get("http://localhost:8080/loadCata").subscribe((data) => this.loadCatag(data));
       this.http.get("http://localhost:8080/loadSongs").subscribe((data) => this.loadSongs(data));
     }
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+  this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+       console.log('FileUpload:uploaded:', item, status, response);
+       this.filestatus=status;
+       console.log("file status",this.filestatus)
+      // alert('File uploaded successfully');
+       //this.router.navigate[''];
+   };
   }
 
 
+  
+   
 
-  handleFileInput(file:FileList){
-    this.fileToUpload = file.item(0)
-    var reader = new FileReader()
-    reader.onload = (event: any) => {
-      this.imgurl = event.target.result;
-      console.log(this.imgurl)
-    }
-    reader.readAsDataURL(this.fileToUpload);
+  // handleFileInput(file:FileList){
+  //   this.fileToUpload = file.item(0)
+  //   var reader = new FileReader()
+  //   reader.onload = (event: any) => {
+  //     this.imgurl = event.target.result;
+  //     console.log(this.imgurl)
+  //   }
+  //   reader.readAsDataURL(this.fileToUpload);
 
+  // }
+  onFileChange(event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.audioname=file.name;
+    // this.uploadForm.patchValue({
+    //   photo: file
+    // });
+    // this.uploadForm.get('photo').updateValueAndValidity();
+    // console.log(this.uploadForm.get('photo'))
   }
-
-  addSong(event)
+  addSong()
   {
-    event.preventDefault()
-    const target = event.target;
+    console.log("asdsadjkshdjhsdjhsahdjhsajdhjs")
+    //event.preventDefault()
+    //const target = event.target;
     //const cname = target.querySelector('#sname').value;
     /*const obj = {
       name : target.querySelector('#sname').value,
       photo : this.imgurl
     }*/
-    const obj = {
-      name : target.querySelector('#sname').value,
-      filmname :target.querySelector('#fname').value,
-      catagory:this.rename,
-      releasedate:target.querySelector('#redate').value,
-      artists:target.querySelector('#artist').value
-    } 
-    this.http.post(`http://localhost:8080/song/add`,obj).subscribe(res => alert("Successfully Added new song!!!"));
+    // const obj = {
+    //   name : target.querySelector('#sname').value,
+    //   filmname :target.querySelector('#fname').value,
+    //   catagory:this.rename,
+    //   releasedate:target.querySelector('#redate').value,
+    //   artists:target.querySelector('#artist').value
+    // } 
+    const fobj={
+      name:this.audioname
+    }
+    //this.http.post(`http://localhost:8080/song/add`,obj).subscribe(res => alert("Successfully Added new song!!!"));
+    //this.http.post(`http://localhost:8080/song/add`,fobj).subscribe(res => res);
+    this.http.post("http://localhost:8000/song/addaudio",fobj).pipe(map(res => res));
+    console.log("file status",this.filestatus);
   }
 
   
